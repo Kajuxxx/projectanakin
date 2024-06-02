49b375d9bbb1489b211db64402a9a59cf4a5cf3e
Kajuxxx <156968420+Kajuxxx@users.noreply.github.com>
2024-06-03 01:28:57 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const profile = {
         name: "Embrace the darkness 🌑",
-        error: "Ulukanin Anasini Gotunden Sikeyim:<br><br>",
+        error: "Projects Of Anakin:<br><br>",
         profilePicture: "anakinss.jpg",
         views: localStorage.getItem('views') ? parseInt(localStorage.getItem('views')) : 726 // localStorage'dan alınan veya başlangıç değeri
     };
